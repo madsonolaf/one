eebfd0362462d8300d879ad1e6b33420b9562617
Volot <maksahater@gmail.com>
2020-11-01 19:32:51 +0300

    первое приложение

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-const category = 'toys';
+const numberOfFilms = +prompt( 'Сколько фильмов вы смотрели', '');
 
-console.log('htps://someurl.com/' + category);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотреных фильмов', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d  = prompt('На сколько оцените его', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
